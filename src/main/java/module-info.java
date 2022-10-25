f59a0da813335e0489b56d6abaadf03bead9cc87
Thomas <thomasshaw258@gmail.com>
2022-10-25 01:59:49 -0700

    Initial commit.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.battleship {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.battleship to javafx.fxml;
+    exports com.example.battleship;
+}
