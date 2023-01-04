836bbd663263eee6be6cc66f7796af131fb0bd5b
Thomas <thomasshaw258@gmail.com>
2023-01-04 01:34:12 -0800

    Added ability to drag ships to grid, for testing.

---
+++ src/main/java/battleship/ui/BattleshipApplication.java
@@ -0,0 +1,101 @@
+package battleship.ui;
+
+import battleship.Board;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.List;
+
+public class BattleshipApplication extends Application {
+
+    @Override
+    public void start(Stage window) throws Exception {
+
+        BorderPane layout = new BorderPane();
+
+        HBox menu = new HBox();
+
+        Button first = new Button("First View");
+        Button second = new Button("Second View");
+
+        menu.getChildren().addAll(first, second);
+
+        layout.setTop(menu);
+
+
+
+        Pane firstLayout = getMainMenuView();
+        PlayerView pv = new PlayerView(new Board());
+
+        Parent secondLayout = pv.getPreparationView(true);
+
+        first.setOnAction((event) -> layout.setCenter(firstLayout));
+        second.setOnAction((event) -> layout.setCenter(secondLayout));
+
+        layout.setCenter(firstLayout);
+
+        Scene scene = new Scene(layout);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+    private StackPane createView(String text) {
+
+        StackPane layout = new StackPane();
+        layout.setPrefSize(300, 180);
+        layout.getChildren().add(new Label(text));
+        layout.setAlignment(Pos.CENTER);
+
+        return layout;
+    }
+
+    public static void main(String[] args) {
+        launch(BattleshipApplication.class);
+    }
+
+    // Main Menu Scene
+    private Pane getMainMenuView() {
+        Pane layout = new StackPane();
+
+        // TODO: Finish text fonts
+        // Using comic sans as placeholder for now.
+        Label title = new Label("Battleship");
+        Label subtitle = new Label("Created By: Me");
+        title.setFont(new Font("Agency FB Bold", 40));
+        subtitle.setFont(new Font("Comic Sans MS", 15));
+
+        // TODO: move this const somewhere better
+        final double buttonWidth = 100;
+        Button newGameButton = new Button("New Game");
+        Button quitButton = new Button("Quit");
+        newGameButton.setFont(new Font("Comic Sans MS", 15));
+        quitButton.setFont(new Font("Comic Sans MS", 15));
+        quitButton.setOnAction((ActionEvent event) -> {
+            Platform.exit();
+        });
+        newGameButton.setMinWidth(buttonWidth);
+        quitButton.setMinWidth(buttonWidth);
+
+        VBox menu = new VBox();
+        menu.setSpacing(10);
+        menu.setAlignment(Pos.CENTER);
+        menu.setPadding(new Insets(10));
+        menu.getChildren().addAll(title, subtitle, newGameButton, quitButton);
+
+        layout.getChildren().add(menu);
+
+        return layout;
+    }
+}
---
+++ src/main/java/battleship/ui/PlayerView.java
@@ -0,0 +1,233 @@
+package battleship.ui;
+
+import battleship.Board;
+import battleship.Coordinate;
+import battleship.InvalidPlacementException;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerView {
+    // TODO: placing ships, shooting ships, and waiting.
+    // TODO: unplaced ship list.
+    private final Board playerBoard;
+    public static final double GRID_CELL_SIZE = 50;
+    private int shipsPlaced = 0;
+
+    public PlayerView(Board playerBoard) {
+        this.playerBoard = playerBoard;
+    }
+
+    public Parent getPlayerView() throws InvalidPlacementException {
+        // TODO: Implement this.
+        return null;
+    }
+
+    /**
+     * Get the window for when a player is setting their ships' positions.
+     *
+     * @param isPlayerOne true if player one is playing currently, false for player two.
+     * @return the window of the preparation view.
+     */
+    public Parent getPreparationView(boolean isPlayerOne) {
+        BorderPane preparationLayout = new BorderPane();
+        GridPane activePlayerGrid = new GridPane();
+        VBox shipPlacingBox = new VBox();
+
+        // Create active player board grid
+        for (int i = 0; i < Board.DEFAULT_SIZE; i++) {
+            activePlayerGrid.getColumnConstraints().add(new ColumnConstraints(50));
+            activePlayerGrid.getRowConstraints().add(new RowConstraints(50)); // TODO: may want to modify this.
+        }
+
+        // Set letter and number labels
+        for (int i = 0; i < playerBoard.getXSize() + 1; i++) {
+            Label letterLabel = new Label(i > 0 && i < 27 ? String.valueOf((char) (i + 64)) : "");
+            Label numberLabel = new Label(i > 0 ? Integer.toString(i) : "");
+
+            letterLabel.setPrefWidth(GRID_CELL_SIZE);
+            letterLabel.setPrefHeight(GRID_CELL_SIZE);
+            letterLabel.setAlignment(Pos.CENTER);
+
+            numberLabel.setPrefWidth(GRID_CELL_SIZE);
+            numberLabel.setPrefHeight(GRID_CELL_SIZE);
+            numberLabel.setAlignment(Pos.CENTER);
+
+            activePlayerGrid.add(letterLabel, i, 0);
+            activePlayerGrid.add(numberLabel, 0, i);
+        }
+
+        // Add buttons to player board grid
+        for (int y = 0; y < playerBoard.getYSize(); y++) {
+            for (int x = 0; x < playerBoard.getXSize(); x++) {
+                // For dragging functionality
+                StackPane gridCell = new StackPane();
+                gridCell.setPrefSize(GRID_CELL_SIZE, GRID_CELL_SIZE);
+                setOnDragOver(gridCell);
+                setOnDragEntered(gridCell);
+                setOnDragExited(gridCell);
+                setOnDragDropped(gridCell);
+
+                Button button = new Button();
+                button.setPrefSize(GRID_CELL_SIZE, GRID_CELL_SIZE);
+                button.setStyle("-fx-background-radius: 0");
+                gridCell.getChildren().add(button);
+                activePlayerGrid.add(gridCell, x + 1, y + 1);
+            }
+        }
+
+        // Ship placing box.
+        // Make ship buttons and properly size.
+        Button carrier = new Button("Carrier");
+        Button battleship = new Button("Battleship");
+        Button cruiser = new Button("Cruiser");
+        Button submarine = new Button("Submarine");
+        Button destroyer = new Button("Destroyer");
+
+        carrier.setPrefSize(GRID_CELL_SIZE * 5, GRID_CELL_SIZE);
+        battleship.setPrefSize(GRID_CELL_SIZE * 4, GRID_CELL_SIZE);
+        cruiser.setPrefSize(GRID_CELL_SIZE * 3, GRID_CELL_SIZE);
+        submarine.setPrefSize(GRID_CELL_SIZE * 3, GRID_CELL_SIZE);
+        destroyer.setPrefSize(GRID_CELL_SIZE * 2, GRID_CELL_SIZE);
+
+        shipPlacingBox.getChildren().addAll(new Label("Ships to Place"), carrier, battleship, cruiser, submarine, destroyer);
+
+        // TODO: Handling dragging - look over this section.
+        setOnDragDetected(carrier);
+        setOnDragDetected(battleship);
+        setOnDragDetected(cruiser);
+        setOnDragDetected(submarine);
+        setOnDragDetected(destroyer);
+
+        setOnDragDone(carrier);
+        setOnDragDone(battleship);
+        setOnDragDone(cruiser);
+        setOnDragDone(submarine);
+        setOnDragDone(destroyer);
+
+        preparationLayout.setLeft(activePlayerGrid);
+        preparationLayout.setRight(shipPlacingBox);
+
+        return preparationLayout;
+    }
+
+
+    //TODO: Event handlers - clean these up.
+    public void setOnDragDetected(Button source)
+    {
+        source.setOnDragDetected((MouseEvent event) -> {
+            Dragboard db = source.startDragAndDrop(TransferMode.ANY);
+
+            ClipboardContent content = new ClipboardContent();
+            content.putString(source.getText() + "\n" + source.getWidth() / GRID_CELL_SIZE);
+            db.setContent(content);
+
+            event.consume();
+        });
+    }
+
+    public void setOnDragDone(Button source)
+    {
+        source.setOnDragDone((DragEvent event) -> {
+            /* If the ship was successfully placed, clear the ship button from ships to place,
+               and update the grid with the ship visible.
+             */
+            if (event.getTransferMode() == TransferMode.MOVE) {
+                source.setVisible(false);
+                source.setCancelButton(true);
+            }
+
+            event.consume();
+        });
+    }
+
+    //target event handlers
+    public void setOnDragOver(StackPane target)
+    {
+        target.setOnDragOver((DragEvent event) -> {
+            if (event.getGestureSource() != target
+                    && event.getDragboard().hasString()) {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            }
+
+            event.consume();
+        });
+    }
+
+    public void setOnDragEntered(StackPane target)
+    {
+        target.setOnDragEntered((DragEvent event) -> {
+            if (event.getGestureSource() != target
+                    && event.getDragboard().hasString()) {
+                target.setStyle("-fx-background-color: green;");
+            }
+
+            event.consume();
+        });
+    }
+
+    public void setOnDragExited(StackPane target)
+    {
+        target.setOnDragExited((DragEvent event) -> {
+            target.setStyle("-fx-background-color: transparent;");
+
+            event.consume();
+        });
+    }
+
+    public void setOnDragDropped(StackPane target)
+    {
+        target.setOnDragDropped((DragEvent event) -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+
+            // TODO: This is quite messy.
+            Button buttonAtTarget = (Button) target.getChildren().get(target.getChildren().size() - 1);
+            if (db.hasString() && buttonAtTarget.getText().isEmpty()) {
+                shipsPlaced++;
+                String[] buttonData = db.getString().split("\n");
+                Button shipButton = (Button) target.getChildren().get(target.getChildren().size() - 1);
+                shipButton.setText("S" + shipsPlaced);
+
+                // Communicate to nearby buttons
+                // Todo: currently only does rightwards
+                GridPane grid = (GridPane) target.getParent();
+                int xCoord = GridPane.getColumnIndex(target);
+                int yCoord = GridPane.getRowIndex(target);
+
+                for (int i = 1; i < Math.round(Double.parseDouble(buttonData[1])); i++) { // Minus 1 as we already did the current square.
+                    StackPane cell = (StackPane) getNodeFromGridPane(grid, xCoord + i, yCoord);
+                    if (cell != null) {
+                        Button cellButton = (Button) cell.getChildren().get(cell.getChildren().size() - 1);
+                        cellButton.setText("S" + shipsPlaced);
+                    }
+                }
+
+                target.getChildren().clear();
+                target.getChildren().add(shipButton);
+
+                success = true;
+            }
+            event.setDropCompleted(success);
+
+            event.consume();
+        });
+    }
+
+
+    private Node getNodeFromGridPane(GridPane gridPane, int column, int row) {
+        for (Node node : gridPane.getChildren()) {
+            if (GridPane.getColumnIndex(node) == column && GridPane.getRowIndex(node) == row) {
+                return node;
+            }
+        }
+
+        return null;
+    }
+}
