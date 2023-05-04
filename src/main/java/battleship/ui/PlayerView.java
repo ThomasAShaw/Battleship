1682908d91a783d9490f797c8311776b5a4061ac
Thomas <thomasshaw258@gmail.com>
2023-05-03 22:57:13 -0700

    Implemented better waiting view + kill time button :)

--- src/main/java/battleship/ui/PlayerView.java
+++ src/main/java/battleship/ui/PlayerView.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import java.util.Objects;
+import java.util.Random;
 
 public class PlayerView {
     // Specific to one player, for one game.
@@ -48,24 +50,51 @@ public class PlayerView {
                 case END:
                     break;
                 }
-            } else {
-            // TODO: Game over screen...
-        }
+            }
         System.out.println(currentState.toString());
         return null; // TODO: Error screen.
     }
 
     private Scene getWaitingView() {
-        VBox waitingMenu = new VBox();
+        VBox waitingMenu = new VBox(30);
         waitingMenu.setAlignment(Pos.CENTER);
+        waitingMenu.getStyleClass().add("waiting-menu");
 
-        Label waitingLabel = new Label("Ready to begin your turn Player " + (isPlayerOne ? "One" : "Two") + "?");
-        Label privacyLabel = new Label("No peeking, Player " + (!isPlayerOne ? "One" : "Two" + "!"));
+        Label waitingLabel = new Label("Ready to begin your turn\nPlayer " + (isPlayerOne ? "One" : "Two") + "?");
+        waitingLabel.getStyleClass().add("waiting-label");
+
+        Label privacyLabel = new Label("No peeking, Player " + (!isPlayerOne ? "One" : "Two") + "!");
+        privacyLabel.getStyleClass().add("privacy-label");
+
         Button beginTurnButton = new Button("Begin Turn");
-        beginTurnButton.setOnAction((event) -> app.switchScene(getPlayerView())); // TODO: Fix this.
+        beginTurnButton.getStyleClass().add("begin-turn-button");
+        beginTurnButton.setOnAction((event) -> app.switchScene(getPlayerView()));
 
-        waitingMenu.getChildren().addAll(waitingLabel, privacyLabel, beginTurnButton);
+        Button killTimeButton = new Button("Kill Time");
+        killTimeButton.setOnAction((event) -> waitingMenu.setStyle("-fx-background-color: " + generateRandomColor()));
 
-        return new Scene(waitingMenu, 1000, 750);
+        waitingMenu.getChildren().addAll(waitingLabel, privacyLabel, beginTurnButton, killTimeButton);
+
+        Scene waitingScene = new Scene(waitingMenu, 1000, 750);
+        waitingScene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/battleship.css")).toExternalForm());
+
+        return waitingScene;
+    }
+
+    private String generateRandomColor() {
+        Random random = new Random();
+        int red = random.nextInt(256);
+        int green = random.nextInt(256);
+        int blue = random.nextInt(256);
+
+        int redSecond = red + random.nextInt(30, 80);
+        redSecond = redSecond > 256 ? redSecond - 256 : redSecond;
+        int greenSecond = green + random.nextInt(30, 80);
+        greenSecond = greenSecond > 256 ? greenSecond - 256 : greenSecond;
+        int blueSecond = blue + random.nextInt(30, 80);
+        blueSecond = blueSecond > 256 ? blueSecond - 256 : blueSecond;
+
+        return String.format("linear-gradient(to bottom, rgba(%d, %d, %d, 1), rgba(%d, %d, %d, 1))", red, green, blue, redSecond, greenSecond, blueSecond);
+
     }
 }
